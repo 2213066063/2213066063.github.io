071ba9e83a9d1a940f21a9a90437ae3c6800577e
2213066063 <124933602+2213066063@users.noreply.github.com>
2024-02-06 16:04:41 -0600

    SEGUNDO COMMIT
    COMMIT2

--- js/script.js
+++ js/script.js
@@ -1,27 +1,14 @@
-// functions.js
+document.addEventListener('DOMContentLoaded', function() {
+    const body = document.body;
+    body.classList.add('show'); // Añade la clase 'show' para activar la transición de opacidad
+});
 
-// Función para cambiar el color de fondo del encabezado al hacer clic
 function cambiarColorFondoHeader() {
-    var header = document.getElementById('header');
-    header.style.backgroundColor = getRandomColor();
-}
-// Función para cambiar el color de fondo de la barra morada al desplazarse hacia abajo
-function cambiarColorFondoBarraMorada() {
-    var purpleBar = document.querySelector('.purple-bar');
-
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > 0) {
-            purpleBar.style.backgroundColor = getRandomColor();
-        }
-    });
+    // Tu lógica actual para cambiar el color del fondo
 }
 
-// Función para obtener un color aleatorio en formato hexadecimal
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+function mostrarAlertaEnImagen() {
+    // Tu lógica actual para mostrar alerta en la imagen
 }
+
+
